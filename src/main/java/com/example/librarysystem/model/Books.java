492a3fdb58065be3831eb044360a39b8a93c689b
Intesar <Godfather.9724>
2021-11-12 15:08:16 +0600

    changes

--- src/main/java/com/example/librarysystem/model/Books.java
+++ src/main/java/com/example/librarysystem/model/Books.java
@@ -1,11 +1,15 @@
 package com.example.librarysystem.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
 @Entity
-
+@Getter
+@Setter
 public class Books implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -13,68 +17,7 @@ public class Books implements Serializable {
     private Long id;
     private String name;
     private String author;
-    private Date year;
+    private String year;
     private String imageUrl;
 
-    public Books() {}
-
-    public Books(String name, String author, Date year, String imageUrl) {
-        this.name = name;
-        this.author = author;
-        this.year = year;
-        this.imageUrl = imageUrl;
-    }
-
-    public Books(Books books) {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public Date getYear() {
-        return year;
-    }
-
-    public void setYear(Date year) {
-        this.year = year;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", author='" + author + '\'' +
-                ", year='" + year + '\'' +
-                ", imageUrl='" + imageUrl + '\'' +
-                '}';
-    }
 }
